d436d27f7bcdd00a2344476b388f9b55cc4b6055
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-16 22:49:22 +0100

    Created jobs.json

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
     <Navbar />
     <Hero />
     <HomeCards />
+    <JobsListings />
 </template>
 
 <script setup lang="ts">
     import Navbar from './components/Navbar.vue';
     import Hero from './components/Hero.vue';
     import HomeCards from './components/HomeCards.vue';
+    import JobsListings from './components/JobsListings.vue';
 </script>
 
 <style scoped>
---
+++ src/components/JobsListings.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Jobs Listings</h2>
+</template>
+
+<script setup lang="ts">
+  import jobData from '../jobs.json';
+  import { ref } from 'vue';
+
+  const jobs = ref(jobData);
+  console.log(jobs.value);
+</script>
