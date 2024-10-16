55dd4193e5a0c3d05fd50cebca5a8a5242cfb85d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-16 18:43:51 +0100

    Created Card.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <Navbar />
     <Hero />
+    <HomeCards />
 </template>
 
 <script setup lang="ts">
     import Navbar from './components/Navbar.vue';
     import Hero from './components/Hero.vue';
+    import HomeCards from './components/HomeCards.vue';
 </script>
 
 <style scoped>
