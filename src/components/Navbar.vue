01c296604991c29885f17ee25edc6271b907092f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-16 17:51:02 +0100

    Created Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="bg-customBlue-300 border-b border-customBlue-600">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+            <img class="h-10 w-auto rounded-md" :src="logo" alt="Vue Jobs" />
+          </a>
+          <div class="md:ml-auto">
+            <div class="flex space-x-2">
+              <a
+                href="index.html"
+                class="text-white bg-customBlue-500 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >Home</a
+              >
+              <a
+                href="jobs.html"
+                class="text-white hover:bg-customBlue-500 hover:text-white rounded-md px-3 py-2"
+                >Jobs</a
+              >
+              <a
+                href="add-job.html"
+                class="text-white hover:bg-customBlue-500 hover:text-white rounded-md px-3 py-2"
+                >Add Job</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+  import logo from '../assets/job-search.gif';
+</script>
