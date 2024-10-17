7b4b9c5f661ebbf0ba36087fe432bd2e4de8099c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-17 11:01:34 +0100

    Updated JobsListenings.vue

--- src/components/JobsListings.vue
+++ src/components/JobsListings.vue
@@ -1,9 +1,19 @@
 <template>
-  <h2>Jobs Listings</h2>
+  <section class="bg-customBlue-400 px-4, py-10">
+    <div class="container-xl lg:container m-auto" >
+      <h2 class="text-3xl font-bold text-customBlue-900 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing v-for="job in jobs" :key="job.id" :job="job"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
   import jobData from '../jobs.json';
+  import JobListing from './JobListing.vue';
   import { ref } from 'vue';
 
   const jobs = ref(jobData);
