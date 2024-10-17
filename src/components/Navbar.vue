91fe8d883ce277c9e4bceea5aa386fda10f0f0ba
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-17 13:00:45 +0100

    Updated paths in Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,12 +16,12 @@
                 >Home</a
               >
               <a
-                href="jobs.html"
+                href="/jobs"
                 class="text-white hover:bg-customBlue-600 hover:text-white rounded-md px-3 py-2"
                 >Jobs</a
               >
               <a
-                href="add-job.html"
+                href="/jobs/add"
                 class="text-white hover:bg-customBlue-600 hover:text-white rounded-md px-3 py-2"
                 >Add Job</a
               >
