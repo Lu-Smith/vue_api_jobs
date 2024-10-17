b7cf5ba24635d9437bddcae639e226dff3e46d24
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-17 13:57:11 +0100

    Updated path in JobListing.vue

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -27,7 +27,7 @@
           {{ job.location }}
         </div>
         <RouterLink
-          :to="'/job/' + job.id"
+          :to="'/jobs/' + job.id"
           class="h-[36px] bg-customBlue-800 hover:bg-customBlue-700 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
