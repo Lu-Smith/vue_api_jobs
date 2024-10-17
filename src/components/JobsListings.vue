4d4cbc4822d5926d19f694829ab4ca86602cbfb5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-17 11:15:01 +0100

    Passed limit prop from App.vue to JobsListemings.vue

--- src/components/JobsListings.vue
+++ src/components/JobsListings.vue
@@ -5,7 +5,9 @@
         Browse Jobs
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing v-for="job in jobs" :key="job.id" :job="job"/>
+        <JobListing 
+        v-for="job in jobs.slice(0, limit || jobs.length)" 
+        :key="job.id" :job="job"/>
       </div>
     </div>
   </section>
@@ -15,6 +17,10 @@
   import jobData from '../jobs.json';
   import JobListing from './JobListing.vue';
   import { ref } from 'vue';
+
+  defineProps({
+    limit: Number
+  })
 
   const jobs = ref(jobData);
   console.log(jobs.value);
