5de3e32ef769d2df80c4cd21a6685a7e7f323097
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-19 19:25:01 +0100

    Created BackButton.vue

---
+++ src/components/BackButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+      <div class="container m-auto py-6 px-6">
+        <RouterLink
+          to="/jobs"
+          class="text-customBlue-600 hover:text-customBlue-500 flex items-center"
+        >
+          <i class="pi pi-arrow-circle-left mr-3"></i> Back to Job Listings
+        </RouterLink>
+      </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+</script>
